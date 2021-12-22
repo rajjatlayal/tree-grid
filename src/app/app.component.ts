8888998a39381d42f9a57e38be3f37676fd68349
Seema Garg <amanlayalupwork@gmail.com>
2021-12-22 18:50:46 +0530

    column edit and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { dataSource, sampleData, virtualData } from './sampledata';
 import { BeforeOpenCloseMenuEventArgs, ContextMenuComponent, MenuEventArgs, MenuItemModel } from '@syncfusion/ej2-angular-navigations';
-import { Component, Inject, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommandColumnService, ContextMenuService, EditService, FilterService, PageService, ToolbarService, TreeGridComponent, VirtualScrollService } from '@syncfusion/ej2-angular-treegrid';
 import { ChangeEventArgs, DropDownList, DropDownListComponent } from '@syncfusion/ej2-angular-dropdowns';
-import { Browser, closest, createElement } from '@syncfusion/ej2-base';
-import { DialogEditEventArgs, EditSettingsModel, SaveEventArgs } from '@syncfusion/ej2-angular-grids';
+import { Browser, closest, createElement, EmitType } from '@syncfusion/ej2-base';
+import { Column, DialogEditEventArgs, EditEventArgs, EditSettingsModel, SaveEventArgs } from '@syncfusion/ej2-angular-grids';
 import { FormGroup } from '@angular/forms';
 
-import { createCheckBox } from '@syncfusion/ej2-buttons';
+import { DialogComponent } from '@syncfusion/ej2-angular-popups';
 
 @Component({
     selector: 'app-root',
@@ -21,13 +21,20 @@ export class AppComponent {
     public text:any;
     public editColumn=false
     public editingColumn=false
+    @ViewChild("deleteDialog")
+  deleteDialogObj!: DialogComponent;
 
     @ViewChild('contextmenu')
     public contextmenu!: ContextMenuComponent;
     @ViewChild('contextmenu')
     public contextmenu2!: ContextMenuComponent;
     addData='false'
-
+    public target: string = '#target';
+    @ViewChild('container', { read: ElementRef, static: true })
+  container!: ElementRef;
+    // The Dialog shows within the target element.
+    // The Dialog shows within the target element.
+  public targetElement!: HTMLElement;
   public editing!: EditSettingsModel;
   isInitialLoad = true;
     public menuItems: MenuItemModel[] = [
@@ -107,7 +114,8 @@ export class AppComponent {
     public taskForm!: FormGroup;
      @ViewChild('treegrid')
   public treegrid!: TreeGridComponent;
-  
+  @ViewChild("editDialog") dialogObj!: DialogComponent;
+
   @ViewChild('dropdown1')
     public dropdown1!: DropDownListComponent;
     pageSettings: { pageCount: 5; } | undefined;
@@ -115,6 +123,8 @@ export class AppComponent {
     isCommandClick: any;
     content: string | undefined;
     content1!: string;
+    public deleteButtons!: ({ click: any; buttonModel: { content: string; isPrimary: boolean; }; } | { click: any; buttonModel: { content: string; isPrimary?: undefined; }; })[];
+    public flag = false;
 
     editParams: { params: { format: string; }; } | undefined;
   contextMenuItems!: (string | { text: string; iconCss: string; target: string; id: string; })[];
@@ -132,17 +142,7 @@ export class AppComponent {
 
 
     ngOnInit(): void {
-        // this.data = virtualData;
-        // this.editSettings ={ allowEditing: true, allowAdding: true, allowDeleting: true, mode:"Cell"}; 
-        // this.toolbar = ['Add', 'Delete', 'Update', 'Cancel'];
-        // this.taskidrules = { required: true , number: true};
-        // this.tasknamerules = { required: true};
-        // this.startdaterules = { date: true};
-        // this.durationrules = {number: true , min: 0};
-        // this.edit = { params: {  format: 'n'}};
-        // this.ddlfields = { text: 'name' , value: 'id'};
-        // this.d1data= [{ id: 'CellEditing', name: 'Cell Editing' }, {id: 'RowEditing', name: 'Row Editing'} ]
- 
+     
         if (virtualData.length === 0) {
           dataSource();
       }
@@ -189,10 +189,17 @@ export class AppComponent {
           id: 'selection',
         },
       ];
-
+      this.deleteButtons = [
+        {
+          click: this.deleteDlgOKButtonClick.bind(this),
+          buttonModel: { content: "Ok", isPrimary: true }
+        },
+        {
+          click: this.deleteDlgCancelButtonClick.bind(this),
+          buttonModel: { content: "Cancel" }
+        }
+      ];
    
-      // this.editing = { allowDeleting: true, allowEditing: true };
-      // this.filterSettings = { type: 'FilterBar', hierarchyMode: 'Parent', mode: 'Immediate' };
 
     }
 
@@ -214,179 +221,46 @@ export class AppComponent {
     this.saveUsername = value;
 }
 
-OpenPopUp(){
-    console.log('data')
-}
-
-openCustomDialog() {}
-   
-
-
-    // Event triggers once the context menu rendering is completed.
-    onCreated(): void {
-        if (Browser.isDevice) {
-            this.content = 'Touch hold to open the ContextMenu';
-            this.contextmenu.animationSettings.effect = 'ZoomIn';
-        } else {
-            this.content = 'Menu 1';
-            this.contextmenu.animationSettings.effect = 'SlideDown';
-        }
-    }
 
 
 
-    onCreated1(): void {
-        if (Browser.isDevice) {
-            this.content1 = 'Touch hold to open the ContextMenu';
-            this.contextmenu.animationSettings.effect = 'ZoomIn';
-        } else {
-            this.content1 = 'Menu 2';
-            this.contextmenu.animationSettings.effect = 'SlideDown';
-        }
-    }
-      // Event triggers while rendering each menu item where “Link” menu item is disabled
-      public addDisabled  (args: MenuEventArgs) {
-
-    
-
-
-
-
-        console.log('data',args.item.text)
-        if (args.item.text === 'Link') {
-            args.element.classList.add('e-disabled');
-        }
-        else if(args.item.text === 'EditCol'){
-            console.log('date')
-            this.data = virtualData;
-            this.editColumn=false
-            this.pageSettings = { pageCount: 5 };
-
-            this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true , mode: 'Dialog' , newRowPosition: 'Below'};
-            // this.toolbar = ['Add', 'Edit', 'Delete'];
-            this.pageSettings = { pageCount: 5 };
-            this.editParams = {  params: { format: 'n' } };
-            this.tasknamerules = { required: true};
-            this.taskidrules = { required: true};
-            this.startdaterules = { date: true};
-            this.enddaterules = { date: true};
-            
-            
-            
-            // this.taskidrules = { required: true , number: true};
-            // this.tasknamerules = { required: true};
-            // this.startdaterules = { date: true};
-            this.durationrules = {number: true , min: 0};
-            // this.edit = { params: {  format: 'n'}};
-            this.ddlfields = { text: 'name' , value: 'id'};
-            this.d1data= [{ id: 'CellEditing', name: 'Cell Editing' }, {id: 'RowEditing', name: 'Row Editing'} ]
-
-        }
-
-        // else if(args.item.text=='FilterCol'){
-        //     console.log('FilterCol')
-
-        // }
-        else if (args.item.text ==='FilterCol'){
-            console.log('date11',this.editColumn)
-            // this.editColumn=true
-            this.editColumn=true
-
-            this.data = virtualData;
-            this.pageSettings = { pageCount: 5 };
-
-            this.filterSettings = { type: 'FilterBar', hierarchyMode: 'Parent', mode: 'Immediate' };
-            this.templateOptions = {
-                create: (args: { element: Element }) => {
-                    let dd: HTMLInputElement = document.createElement('input');
-                    dd.id = 'duration';
-                    return dd;
-                },
-                write: (args: { element: Element }) => {
-                    let dataSource: string[] = ['All', '1', '3', '4', '5', '6', '8', '9'];
-                    this.dropDownFilter = new DropDownList({
-                        dataSource: dataSource,
-                        value: 'All',
-                        change: (e: ChangeEventArgs) => {
-                            let valuenum: any = +e.value;
-                            let id: any = <string>this.dropDownFilter.element.id;
-                            let value: any = <string>e.value;
-                            if ( value !== 'All') {
-                                this.treegrid.filterByColumn( id, 'equal', valuenum );
-                            } else {
-                                this.treegrid.removeFilteredColsByField(id);
-                            }
-                        }
-                    });
-                    this.dropDownFilter.appendTo('#duration');
-             }
-            };
-
-            this.fields1 = { text: 'mode' , value: 'id'};
-            this.d1data= [{ id: 'Parent', mode: 'Parent' },
-                          { id: 'Child', mode: 'Child' },
-                          { id: 'Both', mode: 'Both' },
-                          { id: 'None', mode: 'None' },]
-        }
-        else if(args.item.text==='NewCol'){
-            console.log('NewCol',args)
-            this.editColumn=false
-
-              var obj = { field: "priority", headerText: 'NewColumn', width: 120 }; 
-            //    this.treegrid.columns.push(obj as any);   //you can add the columns by using the treeGrid columns method     
-                  this.treegrid.refreshColumns();   
-                    }
-        else if(args.item.text==='DelCol'){
-            console.log('DelCol',args)
-            // this.treegrid.columns.filter((i,x) => {  
-            //     if(i.field == 'duration') { 
-            //     this.treegrid.columns.splice(x,1); //you can simply remove based on field name or an index of a column 
-            // } 
-            // }); 
-            // this.treegrid.refreshColumns(); 
-
-        }
-             
-                    
-        
-
-    }
-    public addDisabled1  (args: MenuEventArgs) {
-    }
-
-    // public rowSelected(args:any) {
-    //     if (this.isCommandClick) {
-    //       debugger;
-    //       this.isCommandClick = false;
-    //       args.cancel = true;
+    // actionBegin(e: { requestType: string; rowData: any; cancel: boolean; }) {      if (!this.flag) {
+    //     if (e.requestType == "beginEdit") {
+    //       var editedData = e.rowData;
+    //       e.cancel = true;
+    //       // this.OrderID = editedData.OrderID;
+    //       // this.ShipCity = editedData.ShipCity;
+    //       // this.ShipCountry = editedData.ShipCountry;
+    //       this.dialogObj.show();
+    //     }
+    //     if (e.requestType == "delete") {
+    //       e.cancel = true;
+    //       this.deleteDialogObj.show();
     //     }
     //   }
+    // }
+  
+    // actionComplete(e: { requestType: string; }) {
+    //   if (e.requestType === "save" || e.requestType === "delete") {
+    //     this.flag = false;
+    //   }
+    // }
+  
+    // beforeopen(args: { maxHeight: string }) {
+    //   args.maxHeight = '300px';
+    // }
+  
+  
+    deleteDlgOKButtonClick(args: any) {
+      this.deleteDialogObj.hide();
+      this.flag = true;
+      this.treegrid.deleteRecord();
 
-      actionBegin(args: SaveEventArgs): void {
-        if (args.requestType === 'beginEdit' || args.requestType === 'add') {
-            this.taskdata = Object.assign({}, args.rowData);
-        }
-        if (args.requestType === 'save') {
-            if (this.taskForm.valid) {
-                args.data = this.taskdata;
-            } else {
-                args.cancel = true;
-            }
-        }
     }
-
-    actionComplete(args: DialogEditEventArgs): void {
-        if (args.requestType === 'beginEdit' || args.requestType === 'add') {
-            // Set initail Focus
-            if (args.requestType === 'beginEdit') {
-                // (args.form.elements.namedItem('taskName') as HTMLInputElement).focus();
-            } else if (args.requestType === 'add') {
-                // (args.form.elements.namedItem('taskID') as HTMLInputElement).focus();
-            }
-
-        }
+  
+    deleteDlgCancelButtonClick(args: any) {
+      this.deleteDialogObj.hide();
     }
-
 
 
     change (e: ChangeEventArgs) : void {
@@ -465,6 +339,7 @@ openCustomDialog() {}
 
       if (args.item.text == 'EditCol') {
         this.editColumn=false
+        console.log('this.editingColumn',this.editingColumn)
 
         console.log('checkbox.checked',checkbox.checked)
 
@@ -472,32 +347,39 @@ openCustomDialog() {}
           console.log('args.item.text1',args.item.text)
 
           if (this.treegrid.getSelectedRecords().length) {
+            console.log('args.item.text11',args.item.text)
 
             // this.treegrid.startEdit();
+            if( this.editingColumn==false){
+              this.editingColumn=true
   
-           this.editingColumn=true
-  
-            this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true , mode: 'Dialog' };
-            this.pageSettings = { pageCount: 5 };
-            this.editParams = {  params: { format: 'n' } };
-            // this.tasknamerules = { required: true};
-            // this.taskidrules = { required: true};
-            // this.startdaterules = { date: true};
-            // this.enddaterules = { date: true};
-          
-            // this.durationrules = {number: true , min: 0};
-            this.ddlfields = { text: 'name' , value: 'id'};
-            this.d1data= [{ id: 'CellEditing', name: 'Cell Editing' }, {id: 'RowEditing', name: 'Row Editing'} ]
-  
-            // checkbox.checked = !checkbox.checked;
-  
-           checkbox.checked=true
-          } else {
-            alert('Select any row');
-            checkbox.checked=false
-            this.editingColumn=false
-            this.editSettings={}
-          }
+              this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true , mode: 'Dialog' };
+              this.pageSettings = { pageCount: 5 };
+              this.editParams = {  params: { format: 'n' } };
+              // this.tasknamerules = { required: true};
+              // this.taskidrules = { required: true};
+              // this.startdaterules = { date: true};
+              // this.enddaterules = { date: true};
+            
+              // this.durationrules = {number: true , min: 0};
+              this.ddlfields = { text: 'name' , value: 'id'};
+              this.d1data= [{ id: 'CellEditing', name: 'Cell Editing' }, {id: 'RowEditing', name: 'Row Editing'} ]
+    
+              // checkbox.checked = !checkbox.checked;
+    
+             checkbox.checked=true
+            } else {
+              alert('Select any row');
+              checkbox.checked=false
+              this.editingColumn=false
+              this.editSettings={}
+            }
+            }
+  else{
+    this.editingColumn=false
+
+  }
+    
         }
         else{
           console.log('checkbox.checked111',checkbox.checked)
@@ -554,8 +436,46 @@ openCustomDialog() {}
                       { id: 'Both', mode: 'Both' },
                       { id: 'None', mode: 'None' },]
     }
+    else if (args.item.text ==='NewCol'){
+   
+
+
     }
 
+    else if (args.item.text ==='DelCol'){
+      this.editingColumn=false
+      this.editColumn=false
+
+      if(checkbox.checked==true){
+
+        if (this.treegrid.getSelectedRecords().length) {
+          this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
+       
+          this.initilaizeTarget();
+    
+          this.deleteDialogObj.show();
+          checkbox.checked=false
+
+        }
+        else{
+          alert('Select any row');
+          checkbox.checked=false
+          // this.editSettings = { allowEditing: false, allowAdding: false, allowDeleting: false, mode: 'Dialog' };
+  
+        }
+      }
+      else{
+        checkbox.checked=false
+        this.editSettings = { allowEditing: false, allowAdding: false, allowDeleting: false , mode: 'Dialog' };
+
+        // this.editSettings = { allowEditing: false, allowAdding: false, allowDeleting: false, mode: 'Dialog' };
+  
+      }
+    }
+    }
+    public initilaizeTarget: EmitType<object> = () => {
+      this.targetElement = this.container.nativeElement.parentElement;
+        }
 }
 
 export interface ITaskModel {
